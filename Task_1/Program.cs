aec930d621569b4eeea76b2953952719cc019521
Polina Kulikova <pomidorishna@gmail.com>
2023-01-07 19:05:50 +0300

    Задача 1 готова

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 2;
+int b = 10;
+int max = a;
+
+if (b > a) max = b;
+
+Console.Write( "max = ");
+Console.WriteLine(max);
+
+
+
+
