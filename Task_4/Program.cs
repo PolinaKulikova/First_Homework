10ad7250a77d26252f9e70a70b491b6ffe4d59fb
Polina Kulikova <pomidorishna@gmail.com>
2023-01-08 19:17:58 +0300

    Блок-схема 4 готова

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число");
+int N =  Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+
+
+while (count <= N)
+{
+            Console.WriteLine(N);
+            
+}
