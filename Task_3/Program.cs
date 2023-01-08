9520cc5f2e295244949a1043a07d01d7b6800c2a
Polina Kulikova <pomidorishna@gmail.com>
2023-01-08 18:43:00 +0300

    задача 3 решена

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите число");
+int n =  Convert.ToInt32(Console.ReadLine());
+
+if ( n % 2 == 0 )
+{
+        Console.WriteLine("Да");
+}
+else
+{
+        Console.WriteLine("Нет");
+}
